661164f90dddd312d134ca2f3455621385aa2d93
Flyingrepo <162626258+Flyingrepo@users.noreply.github.com>
2024-03-22 08:08:27 +0530

    2b.py

--- 2b.py
+++ 2b.py
@@ -1,15 +1,25 @@
-def BinToDec(b):
-return int(b, 2)
+﻿
 
-print("Enter the Binary Number: ", end="")
-bnum = input()
-dnum = BinToDec(bnum)
-print("\nEquivalent Decimal Value = ", dnum)
-
-def OctToHex(o):
-return hex(int(o, 8))
-
-print("Enter Octal Number: ", end="")
-onum = input()
-hnum = OctToHex(onum)
-print("\nEquivalent Hexadecimal Value =", hnum[2:].upper())
+def bin2Dec(val): rev=val[::-1] dec = 0 i=0
+for dig in rev:
+dec+= int(dig)* 2**i
+i+= 1 return dec
+def oct2Hex(val):
+rev=val[::-1] dec = 0
+i = 0
+for dig in rev:
+dec += int(dig)*8**i i+= 1
+list=[]
+while dec != 0:
+list.append(dec%16) dec = dec // 16
+nl=[]
+for elem in list[::-1]:
+if elem <= 9:
+nl.append(str(elem))
+else:
+nl.append(chr(ord('A') + (elem -10)))
+hex = "" join(nl)
+return hex
+num1 = input("Enter a binary number: ")
+print(bin2Dec(num1))
+num2 = input("Enter a octal number: ") print(oct2Hex(num2))
